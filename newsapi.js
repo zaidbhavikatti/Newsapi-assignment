ed992367f256f3e4a78bdfc1ec012e20fe0669f2
zaidbhavikatti <98631203+zaidbhavikatti@users.noreply.github.com>
2022-04-14 18:14:53 +0530

    first commit

---
+++ newsapi.js
@@ -0,0 +1,161 @@
+let json={
+    status: "ok",
+    totalResults: 10,
+}
+  let articles=[
+    
+   
+Natasha={
+    author : "Natasha Mascarenhas",
+    title: "Crypto’s latest disruption may be investor expectations",
+    description: "Welcome to Startups Weekly, a fresh human-first take on this week’s startup news and trends. To get this in your inbox, subscribe here. I’ve noticed that the long-awaited re-correction of private tech startup valuations and fundraising expectations has a web3…",
+    url: "https://techcrunch.com/2022/04/09/web-2-0-got-the-memo-about-valuations-coming-down-and-web3-has-not/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2021/03/cryptocurrency-coins.jpg?w=600",
+    publishedAt: "2022-04-09T20:38:58Z",
+            content: "Welcome to Startups Weekly, a fresh human-first take on this weeks startup news and trends. To get this in your inbox, subscribe here.\r\nIve noticed that the long-awaited re-correction of private tech… [+5674 chars]"
+  
+},
+console.log("* article about natasha mascarenhas *"),
+console.log('author :', nata.author ),
+console.log('title:',nata.title),
+console.log('description:',nata.description),
+console.log('url :',nata.url),
+
+lucas={
+    author: "Lucas Matney",
+    title: "Will Apple build its own blockchain?",
+    description: "Hello readers, and welcome back! Last week, I wrote about the issues facing Axie Infinity in the wake of a $625 million heist. This week, I’m talking about Apple and crypto. If you like my ramblings, follow me on Twitter and subscribe to my new crypto newslet…",
+    url: "https://techcrunch.com/2022/04/09/will-apple-build-its-own-blockchain/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2022/03/GettyImages-1235257298.jpg?w=600",
+    publishedAt: "2022-04-09T19:24:51Z",
+    content: "Hello readers, and welcome back!\r\nLast week, I wrote about the issues facing Axie Infinity in the wake of a $625 million heist. This week, I’m talking about Apple and crypto.\r\nIf you like my rambling… [+6681 chars]"
+    },
+    console.log(" * article about lucas matney *"),
+    console.log('author :', luca.author ),
+    console.log('title:',luca.title),
+    console.log('description:',luca.description),
+    console.log('url :',luca.url),
+ Sarah={
+    
+    author: "Sarah Perez",
+    title: "This Week in Apps: Elon joins Twitter's board, Apple's subscriptions pilot, WWDC stays online",
+    description: "Welcome back to This Week in Apps, the weekly TechCrunch series that recaps the latest in mobile OS news, mobile applications and the overall app economy.",
+    url: "https://techcrunch.com/2022/04/09/this-week-in-apps-elon-joins-twitters-board-apples-subscriptions-pilot-wwdc-stays-online/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2021/11/this-week-in-apps-splash-2021.png?w=753",
+    publishedAt: "2022-04-09T18:20:38Z",
+    content: "Welcome back to This Week in Apps, the weekly TechCrunch series that recaps the latest in mobile OS news, mobile applications and the overall app economy.\r\nThe app industry continues to grow, with a … [+23887 chars]"
+    },
+    console.log("* article about sarah perez *"),
+    console.log('author :', sara.author ),
+    console.log('title:',sara.title),
+    console.log('description:',sara.description),
+    console.log('url :',sara.url),
+  Alex={
+    
+    author: "Alex Wilhelm",
+    title: "Elon Musk, dual-class shares, and who owns the future",
+    description: "Facebook is struggling to reinvent itself under the same leadership that brought it success. Twitter, in contrast, is now run by a non-founder, and added a controversial power-user to its board.",
+    url: "https://techcrunch.com/2022/04/09/elon-musk-dual-class-shares-and-who-owns-the-future/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2020/06/NSussman_Techcrunch_Exchange-multicolor.jpg?w=533",
+    publishedAt: "2022-04-09T17:01:06Z",
+    content: "Welcome to The TechCrunch Exchange, a weekly startups-and-markets newsletter. Its inspired by the daily TechCrunch+ column where it gets its name. Want it in your inbox every Saturday? Sign up here. … [+3719 chars]"
+    },
+    console.log("* article about alex wilhelm *"),
+    console.log('author :', alex.author ),
+    console.log('title:',alex.title),
+    console.log('description:',alex.description),
+    console.log('url :',alex.url),
+Lisa={
+    
+    author: "Lisa Kaplan, Lee Foster",
+    title: "Disinformation demands a collective defense",
+    description: "As the disinformation threat has evolved, regulations governing the digital space have not kept up, and the agencies that have historically sought to defend us against disinformation face an asymmetry that is difficult to overcome alone.",
+    url: "https://techcrunch.com/2022/04/09/disinformation-demands-a-collective-defense/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2022/04/fakenews.jpg?w=700",
+    publishedAt: "2022-04-09T14:03:34Z",
+    content: "The TechCrunch Global Affairs Project examines the increasingly intertwined relationship between the tech sector and global politics.\r\nWhen the term disinformation went mainstream after the 2016 elec… [+4378 chars]"
+    },
+    console.log("* article about lee foster *"),
+    console.log('author :', lisa.author ),
+    console.log('title:', lisa.title),
+    console.log('description:',lisa.description),
+    console.log('url :', lisa.url),
+Abigail={
+    
+    author: "Abigail Bassett",
+    title: "Inside Mercedes' plan to deliver hands-free driving to the masses",
+    description: "I’m sitting in the passenger seat of a special Mercedes Benz S-Class on a blindingly bright California spring day, casually watching an autonomous delivery robot roll through a crosswalk on its way to deliver someone’s takeout meal in Santa Monica. The test d…",
+    url: "https://techcrunch.com/2022/04/09/test-mercedes-benz-hands-free-driving-technology/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2022/04/mercedes-drive-pilot-system-steering-wheel.jpg?w=600",
+    publishedAt: "2022-04-09T14:00:57Z",
+    content: "I’m sitting in the passenger seat of a special Mercedes Benz S-Class on a blindingly bright California spring day, casually watching an autonomous delivery robot roll through a crosswalk on its way t… [+10290 chars]"
+    },
+    console.log("* article about abigali basset *"),
+    console.log('author :', abi.author ),
+    console.log('title:', abi.title),
+    console.log('description:',abi.description),
+    console.log('url :', abi.url),
+Drew= {
+    
+    author: "Drew Olanoff",
+    title: "Is social media (re)traumatizing you?",
+    description: "Drew Olanoff Contributor Share on Twitter Drew is a geek who first worked at AOL when he was 16 years old. Previously a senior writer at TechCrunch, he has spent time as a communications professional at Yahoo, EyeEm and Scaleworks. More posts by this contribu…",
+    url: "https://techcrunch.com/2022/04/09/is-social-media-retraumatizing-you/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2022/04/GettyImages-1359720343-e1649452046653.jpg?w=621",
+    publishedAt: "2022-04-09T13:15:24Z",
+    content: "More posts by this contributor\r\nWhat happens when youre out of content to scroll through and react to on the internet? Whats there to keep you engaged whether the content makes you angry, sad, happy … [+12212 chars]"
+    },
+    console.log("* article about drew olanoff *"),
+    console.log('author :', drew.author ),
+    console.log('title:', drew.title),
+    console.log('description:',drew.description),
+    console.log('url :', drew.url),
+Dana= {
+    
+    author: "Dana Linnet",
+    title: "Government action on tech innovation is good news for startups",
+    description: "By working together toward success, the Pentagon and Silicon Valley are truly capable of anything, including defending the free world against the worst existential threats.",
+    url: "https://techcrunch.com/2022/04/09/government-action-on-tech-innovation-is-good-news-for-startups/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2022/04/us_tech-flag.jpg?w=667",
+    publishedAt: "2022-04-09T13:13:01Z",
+    content: "The TechCrunch Global Affairs Project examines the increasingly intertwined relationship between the tech sector and global politics.\r\nMuch has been written in this space about the Defense Department… [+7664 chars]"
+    },
+    console.log("* article about dana linnet *"),
+    console.log('author :', dana.author ),
+    console.log('title:', dana.title),
+    console.log('description:',dana.description),
+    console.log('url :', dana.url),
+Jacq= {
+   
+    author: "Jacquelyn Melinek, Natasha Mascarenhas",
+    title: "Crypto is altering the investing landscape for even the most disciplined VCs",
+    description: "Is it hype, is it crypto, or is it both at the same time?",
+    url: "https://techcrunch.com/2022/04/09/crypto-cap-table-venture/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2022/04/GettyImages-1295429677.jpg?w=571",
+    publishedAt: "2022-04-09T13:00:52Z",
+    content: "The long-awaited re-correction of private tech startup valuations and fundraising expectations has a web3-sized asterisk next to it.\r\nWhile many funds are returning to more conservative check-writing… [+3727 chars]"
+    },
+    console.log("* article about Jacquelyn Melinek *"),
+    console.log('author :', jacq.author ),
+    console.log('title:', jacq.title),
+    console.log('description:',jacq.description),
+    console.log('url :', jacq.url),
+ Dave={
+   
+    author: "Dave Mullen",
+    title: "The answers to real estate's climate tech questions may be all around us",
+    description: "For those who do care, there is no shortage of confusion on how to best tackle the looming threat of climate change. But what if an answer was lying right in front of us?",
+    url: "https://techcrunch.com/2022/04/08/the-answers-to-real-estates-climate-tech-questions-may-be-all-around-us/",
+    urlToImage: "https://techcrunch.com/wp-content/uploads/2022/04/GettyImages-1364345964.jpg?w=600",
+    publishedAt: "2022-04-08T23:45:06Z",
+    content: "More posts by this contributor\r\nIf you havent seen Adam McKays “Dont Look Up” starring Meryl Streep, Leonardo DiCaprio and Jennifer Lawrence, you should. The film speaks to an existential, albeit pre… [+3004 chars]"
+    
+}
+
+]  
+console.log (" * article about dave mullen * ")
+    console.log('author :', dave.author )
+    console.log('title:',dave.title)
+    console.log('description:',dave.description)
+    console.log('url :',dave.url)
+   
